d36cb97dfe1145781324ea88a0caf36f395b9f36
vilebile17 <somaurooibrahim@gmail.com>
2025-05-26 13:46:21 +0100

    Finished lesson 13, the hardest one yet!

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from stats import count_words
-from stats import count_characters
-from stats import sort_dict
-from stats import check_alphabet
+from stats import count_words, count_characters, sort_dict, check_alphabet
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -9,13 +6,18 @@ def get_book_text(file_path):
         return file_contents
 
 def main():
-    dictionary = get_book_text("books/frankenstein.txt")
-    frankenstein = get_book_text("books/frankenstein.txt")
+    file_path = "books/frankenstein.txt"
+    frankenstein = get_book_text(file_path)
     word_count = count_words(frankenstein)
     character_count = count_characters(frankenstein)
-    print(f"{word_count} words found in the document")
     character_count = sort_dict(character_count)
-    print(character_count)
-
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count -----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for char in character_count:
+        print(f"{char}: {character_count[char]}")
+    print("============= END ===============")
 
 main()
